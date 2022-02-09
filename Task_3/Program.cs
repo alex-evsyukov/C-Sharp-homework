b4ec5b8a1b31704fc448635d32694fa2fe75f495
Alexander <alexander19.evsyukov97@gmail.com>
2022-02-09 20:49:56 +0300

    Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 31;
+int b = 5;
+if (a > b)
+{
+    Console.WriteLine("max = " + a);
+    Console.WriteLine("min = " + b);
+}
+else
+{
+    Console.WriteLine("max = " + b);
+    Console.WriteLine("min = " + a);
+}
