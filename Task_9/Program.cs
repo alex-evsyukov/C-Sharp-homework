fdf8b170d44cc53896817337e9f5ed48e000d4d7
Alexander <alexander19.evsyukov97@gmail.com>
2022-02-10 22:55:48 +0300

    Task_9

---
+++ Task_9/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать четные числа от 1 до N
+System.Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int a = 0;
+while (a < N)
+{
+    a++;
+    if (a % 2 == 0)
+    {
+        System.Console.WriteLine(a);
+    }
+}
