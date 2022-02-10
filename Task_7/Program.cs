f47481a867b1a27f827768424102bd490b46d303
Alexander <alexander19.evsyukov97@gmail.com>
2022-02-10 21:13:56 +0300

    Task_7

---
+++ Task_7/Program.cs
@@ -0,0 +1,12 @@
+﻿//Выяснить является ли число чётным
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    System.Console.WriteLine("Число четное");
+}
+else
+{
+    System.Console.WriteLine("Число не четное");
+}
