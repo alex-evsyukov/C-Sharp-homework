48557f6290cd1ebbccc56674e4a6b9dc0075bde0
Alexander <alexander19.evsyukov97@gmail.com>
2022-03-10 00:38:12 +0300

    Task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,11 @@
+﻿//Показать последнюю цифру трёхзначного числа
+int a;
+string s;
+Console.Write("Введите число: ");
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+while (a > 10)
+{
+    a %= 10;
+}
+Console.WriteLine(a);
