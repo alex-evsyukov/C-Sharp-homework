577505f8e30f3f7390c01f0101519c5b90376524
Alexander <alexander19.evsyukov97@gmail.com>
2022-03-10 00:38:40 +0300

    Task_11

---
+++ Task_11/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 0;
+string s;
+int y;
+Console.Write("Введите число: ");
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+while (a >= 100)
+{
+    a /= 10;
+}
+y = a % 10;
+System.Console.WriteLine(y);
